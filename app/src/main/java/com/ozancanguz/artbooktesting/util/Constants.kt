fb7c2bd6033c4f03073d27ccf7e2a64d7dbb8f6a
Ozan <ozancangu24@gmail.com>
2023-01-27 18:36:13 +0300

    -constants class created

---
+++ app/src/main/java/com/ozancanguz/artbooktesting/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.ozancanguz.artbooktesting.util
+
+class Constants {
+
+    companion object{
+        const val API_KEY="33167929-960c9e3c2922c091f802ff87b"
+        const val BASE_URL="https://pixabay.com/"
+
+    }
+}
