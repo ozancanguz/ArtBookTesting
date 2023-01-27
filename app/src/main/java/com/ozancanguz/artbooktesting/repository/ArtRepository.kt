ab20222328a2fc4d833d8b61de3455914e293d4b
Ozan <ozancangu24@gmail.com>
2023-01-27 19:18:37 +0300

    -resource class created for repository

---
+++ app/src/main/java/com/ozancanguz/artbooktesting/repository/ArtRepository.kt
@@ -0,0 +1,4 @@
+package com.ozancanguz.artbooktesting.repository
+
+class ArtRepository {
+}
---
+++ app/src/main/java/com/ozancanguz/artbooktesting/repository/ArtRepositoryInterface.kt
@@ -0,0 +1,4 @@
+package com.ozancanguz.artbooktesting.repository
+
+interface ArtRepositoryInterface {
+}
