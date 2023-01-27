210fb55584f887bc950367cbe127ebb6d80b76f3
Ozan <ozancangu24@gmail.com>
2023-01-27 19:27:08 +0300

    -art repository implemented

--- app/src/main/java/com/ozancanguz/artbooktesting/repository/ArtRepository.kt
+++ app/src/main/java/com/ozancanguz/artbooktesting/repository/ArtRepository.kt
@@ -1,4 +1,43 @@
 package com.ozancanguz.artbooktesting.repository
 
-class ArtRepository:ArtRepositoryInterface {
+import androidx.lifecycle.LiveData
+import com.ozancanguz.artbooktesting.data.api.ImageApi
+import com.ozancanguz.artbooktesting.data.db.Art
+import com.ozancanguz.artbooktesting.data.db.ArtDao
+import com.ozancanguz.artbooktesting.data.model.ImageResponse
+import com.ozancanguz.artbooktesting.util.Resource
+import javax.inject.Inject
+
+class ArtRepository@Inject constructor(private val artDao: ArtDao,
+                                       private val imageApi: ImageApi
+                                       ):ArtRepositoryInterface {
+
+    override suspend fun insertArt(art: Art) {
+        artDao.insertArt(art)
+    }
+
+    override suspend fun deleteArt(art: Art) {
+      artDao.deleteArt(art)
+    }
+
+    override fun getArt(): LiveData<List<Art>> {
+        return artDao.observeArt()
+    }
+
+    override suspend fun searchImage(imageString: String): Resource<ImageResponse> {
+
+        return try {
+
+            val response=imageApi.imageSearch(imageString)
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    return@let Resource.success(it)
+                } ?: Resource.error("Error",null)
+            } else {
+                Resource.error("Error",null)
+            }
+        } catch (e: Exception) {
+            Resource.error("No data!",null)
+        }
+    }
 }
