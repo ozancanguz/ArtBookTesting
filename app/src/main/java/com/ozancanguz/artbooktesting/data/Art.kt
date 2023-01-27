d4693e1918b3e89272a92e779dcf9761c0effee6
Ozan <ozancangu24@gmail.com>
2023-01-27 18:24:57 +0300

    -room art data class created

---
+++ app/src/main/java/com/ozancanguz/artbooktesting/data/Art.kt
@@ -0,0 +1,14 @@
+package com.ozancanguz.artbooktesting.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+@Entity(tableName = "arts")
+data class Art(
+    var name:String,
+    var artistName:String,
+    var artYear:Int,
+    var imageUrl:String,
+
+    @PrimaryKey(autoGenerate = true)
+    var id:Int
+)
--- app/src/main/java/com/ozancanguz/artbooktesting/ui/artdetails/ArtDetailsFragment.kt
+++ app/src/main/java/com/ozancanguz/artbooktesting/ui/artdetails/ArtDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ozancanguz.artbooktesting.R
 import com.ozancanguz.artbooktesting.databinding.FragmentArtDetailsBinding
 
@@ -25,6 +26,10 @@ class ArtDetailsFragment : Fragment() {
     ): View? {
           _binding = FragmentArtDetailsBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.artImageView.setOnClickListener {
+            findNavController().navigate(R.id.action_artDetailsFragment_to_imageApiFragment)
+        }
 
 
         return view
